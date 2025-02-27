c930a41cdea3ec8fc7c1a996cebbacea39e0fac4
Bax <chernoff.serg3i@yandex.ru>
2025-02-28 00:46:51 +0300

    update: update database script

--- database/birthday_help_db.sql
+++ database/birthday_help_db.sql
@@ -1,10 +1,6 @@
--- данные пользователя, псевдоним, дата через которую напомнить (id), дни рождения (которые введены или будут введены - задать вопрос)
--- список подарков - пользователь (id), ссылка на подарок (придумать парсер-api для вб и озон, цена (попробовать сделать автоподстановку)), цена, приоритет
--- приоритет (приоритет, id)
--- дата через которую напомнить (дата, id)
--- список пользователей - друзей
-
 --drop table user_data
+--drop table user_friends
+--drop table users_gifts
 
 create table user_data(
 	id_user int constraint pk_user primary key constraint check_user_id check (id_user > 0),
@@ -13,4 +9,18 @@ create table user_data(
 	interval_lenth_day int constraint check_interval_lenth check (interval_lenth_day >= 0 AND interval_lenth_day <= 30)
 )
 
+create table user_friends(
+	id_row uuid constraint pk_user_friends primary key constraint default_id_ro default  gen_random_uuid(),
+	id_user int not null,
+	friend int not null,
+	constraint fk_main_user foreign key (id_user) references user_data(id_user) on delete cascade,
+	constraint fk_friend_user foreign key (friend) references user_data(id_user) on delete cascade
+)
 
+create table users_gifts(
+	id_row uuid constraint pk_users_gifts primary key constraint default_id_ro default  gen_random_uuid(),
+	id_user int not null,
+	name_gift text not null constraint check_name_gift check (char_length(name_gift) > 0),
+	link_gift text,
+	constraint fk_id_user foreign key (id_user) references user_data(id_user) on delete cascade
+)
